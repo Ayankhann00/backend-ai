0d6f0865634b3350aed6d19db1def3966dc976ef
AYAN KHAN <153628789+Ayankhann00@users.noreply.github.com>
2026-07-22 16:29:19 +0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,18 +2,65 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello Backend AI Internship!",
-  });
+app.use(express.json());
+
+let tasks = [
+  {
+    id: 1,
+    title: "Learn Backend",
+    completed: false,
+  },
+];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
 
-app.get("/status", (req, res) => {
-  res.json({
-    status: "OK",
-  });
+app.get("/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t.id === parseInt(req.params.id));
+
+  if (!task) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  res.json(task);
+});
+
+app.post("/tasks", (req, res) => {
+  const { title } = req.body;
+
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  res.status(201).json(newTask);
+});
+
+
+app.put("/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t.id === parseInt(req.params.id));
+
+  if (!task) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  task.title = req.body.title ?? task.title;
+  task.completed =
+    req.body.completed !== undefined ? req.body.completed : task.completed;
+
+  res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  tasks = tasks.filter((t) => t.id !== parseInt(req.params.id));
+
+  res.json({ message: "Deleted Task" });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
